f76194a7bf432d4060dba950d276f3af9a95270f
habib <habib@habibs-MacBook-Pro.local>
2022-03-12 14:49:32 +0000

    first react commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./NewApp.css";
 
 function App() {
+  const name = "habib";
+  const myName = <h2>my name is {name}</h2>;
+
+  const boy ='kwaku'
+  const multiline = (
+    <>
+      <div>
+        <h1>hello u</h1>
+        <hr/>
+        <p>lorem iposum jkknas;oaihibcbiubs;{boy}</p>
+      </div>
+    </>
+  );
+
+  const abigail = <p> i am a girl</p>;
+  const multi = (
+    <>
+      <div>
+        <h1>hello u\bkb</h1>
+        <p>lorem iposum jkknas;oaihibcbiubs;</p>
+      </div>
+    </>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: "yellow", backgroundColor: "blue", marginTop: 50 }}>
+        {" "}
+        hello world
+      </h1>
+      {abigail}
+
+      <h5 style={{fontSize:50,color:'grey'}}> boy</h5>
+
+      <p> i am a boy</p>
+      <br />
+      {myName}
+      <div className="nyarko">
+        <p>jdififsgig</p>
+      </div>
+      {multiline}
+      <img src="hgh" />
+      <a href="nbjkabjkkg" id="hel">
+        {" "}
+        click here
+      </a>
+      {multi}
     </div>
   );
 }
