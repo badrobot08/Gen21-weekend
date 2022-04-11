ddb0e13749a88fc519766569777d69f26667dd2c
habbkad <habibkadiri52@gmail.com>
2022-04-11 20:28:34 +0000

    Done eith crud

--- src/App.js
+++ src/App.js
@@ -3,34 +3,61 @@ import React, { useState } from "react";
 import Form from "./CRUD BootStrap/FormBootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FormList from "./CRUD BootStrap/FormList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [persons, setpersons] = useState([
     {
       name: "Joel",
       description: "Like coding",
+      id: uuidv4(),
     },
     {
       name: "Frank",
       description: "Like programming",
+      id: uuidv4(),
     },
   ]);
 
-  console.log(persons);
+  //console.log(persons);
 
   const updateState = (userName, userDescription) => {
     const newPerson = {
       name: userName,
       description: userDescription,
+      id: uuidv4(),
     };
 
     setpersons([...persons, newPerson]);
   };
 
+  //deletes the element with the same id
+  const deletePerson = (id) => {
+    const newPerson = persons.filter((person) => person.id !== id);
+    setpersons(newPerson);
+    // console.log(newPerson);
+  };
+
+  //edits the user data already present in the array
+  const editPerson = (id, modifiedPerson) => {
+    const newPerson = persons.map((person) =>
+      person.id === id ? modifiedPerson : person
+    );
+    //{
+    //   if (person.id === id) {
+    //     return modifiedPerson;
+    //   } else {
+    //     return person;
+    //   }
+    // });
+
+    setpersons(newPerson);
+  };
+
   return (
     <div className="App">
       <Form getData={updateState} />
-      <FormList listData={persons} />
+      <FormList listData={persons} delete={deletePerson} edit={editPerson} />
     </div>
   );
 }
